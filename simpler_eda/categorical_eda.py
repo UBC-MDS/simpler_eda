5460ad035a0d8507663072fc2707fdd5c3df5e74
Nicholas Wu <misternicholaswu@gmail.com>
2021-02-26 16:54:58 -0800

    update on categorical function docstring

--- simpler_eda/categorical_eda.py
+++ simpler_eda/categorical_eda.py
@@ -4,26 +4,27 @@ import pandas as pd
 
 def categorical_plot(
     data,
-    xval=None,
-    yval=None,
-    plot_type="bar",
-    color=None,
-    title=None,
-    font_size=10,
-    bar_size=5,
-    color_scheme=None,
-    sort_by=None,
-    plot_height=None,
-    plot_width=None,
-    facet=None
+    xval = None,
+    yval = "count",
+    plot_type = "histogram",
+    color = None,
+    title = None,
+    font_size = 10,
+    color_scheme = "yellowgreenblue",
+    plot_height = 150,
+    plot_width = 200,
+    opacity = 0.6,
+    facet = None
+    
 ):
     """
-    This function takes in a data frame object, one categorical column,
-    and one argument to specify count or percentage variable,
-    to produce a bar plot that visualizes the
-    count or percentage value of the feature. User can optionally change
-    default arguements for color, plot title, size of text,
-    color-schem, orientation, plot size and facet factor.
+    This function takes in a data frame object and one categorical
+    feature, to produce a histogram plot that visualizes the
+    distribution of the feature. User can also choose to plot density
+    graph of the feature by specifing in plot_type. 
+    The function also offers customization on color, plot title, 
+    font size, color-scheme, plot size, opacity level and facet 
+    factor.
 
     Parameters
     ----------
@@ -31,45 +32,49 @@ def categorical_plot(
        Input dataframe object.
     xval : str
       Variable used to represent the x-axis.
-    yval : str
+    yval : str, optional
       Variable used to represent the y-axis.
     plot_type : str, optional
-      Variable used to represent the graphical relationship between xval and yval.
+      Variable used to specify plot type. Options include "histogram" 
+      and "density". When "density" is selected, argument yval becomes obsolete.
     color : str, optional
       Variable used to set the color of the marks in the plot object.
     tilte : str, optional
       Variable used to set the title of the plot.
     font_size  : int, optional
       Variable used to set the size of the axis labels and title.
-    bar_size : int, optional
-      Variable used to set the bar size.
     color_scheme : str, optional
       Variable used to set the bar size.
-    sort_by : str, optional
-      Variable used to specify sort argument
     plot_height : int, optional
       Variable used to specify plot height
     plot_witdh : int, optional
       Variable used to specify plot width
+    opacity : float, optional
+      Variable used to specify density fill opacity for density plot
     facet : str, optional
       Variable used to specify facet factor
 
     Returns
     -------
     `altair`
-        A bar chart according to user specification in the parameters.
+        A histogram or density chart object based on user specifications.
 
     Examples
     --------
+    >>> import altair as alt
+    >>> import numpy as np
+    >>> import pandas as pd
     >>> from simpler_eda.categorical_eda import categorical_plot
     >>> from vega_datasets import data
     >>> cars = data.cars()
-    >>> categorical_plot(cars, xval = "Origin", 
+    >>> categorical_plot(data = cars, 
+                        xval = "Origin", 
                         yval = "count()",
                         color = "Horsepower",
                         title = "Histogram of Origin in Different Levels of Horsepower",
-                        sort_by = '-y',
                         plot_height = 100,
                         plot_width = 200
                         )
     """
+
+    return categorical_plot
