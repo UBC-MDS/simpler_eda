8ea5a2e29866c35f05d101bc79d6b99b36b1802c
Gurdeepak Sidhu <gurdeepaksidhu@yahoo.com>
2021-03-05 18:40:29 -0800

    Test function dosctrings and branch coverage

--- simpler_eda/numerical_eda.py
+++ simpler_eda/numerical_eda.py
@@ -7,7 +7,10 @@ import pytest
 from pandas.api.types import is_numeric_dtype
 
 
-def numerical_plot(
+cars = data.cars()
+
+
+def numerical_eda(
     data,
     xval=None,
     yval=None,
@@ -21,91 +24,19 @@ def numerical_plot(
     x_transform=False,
     y_transform=False,
 ):
-    """
-    This function takes in a data frame object, two numeric columns,
-    and produces either a scatter or line plot to visualize the relationship
-    between the two numerical features. Users can optionally change default
-    arguments for plot-type, color, title, size of text, color-scheme, and
-    toggle log transformation for the x and y axis.
-
-    Parameters
-    ----------
-    data : pandas.core.frame.DataFrame
-       Input dataframe object.
-
-    xval : str
-      Variable used to represent the x-axis.
-
-    yval : str
-      Variable used to represent the y-axis.
-
-    plot_type : str
-      Variable used to represent the graphical relationship between xval and yval, options are scatter or line plot.
-
-    color : str
-      Variable either used to group the data ponts in different colors, or specific color for the mark.
-
-    tilte : str, optional
-      Variable used to set the title of the plot.
-
-    font_size  : int, optional
-      Variable used to set the size of the axis labels and title.
-
-    color_scheme: str, optional
-      The color scheme used for the plot.
-      Other color schemes can be "accent", "category10", "category20", "category20b", "dark2", etc.
-      Other proper color scheme reference can be found in https://vega.github.io/vega/docs/schemes/
-
-    plot_width: int, optional
-      The width of the plot.
-
-    plot_height: int, optional
-      The height of the plot.
-
-    x_transform : bool, optional
-      Determines whether a log transformation occurs on the x-axis.
-
-    y_transform : bool, optional
-      Determines whether a log transformation occurs on the y-axis.
-
-    Returns
-    -------
-    `altair`
-      Scatter plot or Line plot of user-specified variables.
-    Examples
-    --------
-    >>> import altair as alt
-    >>> import pandas as pd
-    >>> import numpy as np
-    >>> from simpler_eda.numerical_eda import numerical_plot
-    >>> from vega_datasets import data
-    >>> numerical_plot(data.cars(), xval = "Horsepower", yval = "Acceleration", plot_type = "line",
-                 color = "Origin",
-                 title = " Horsepower vs Acceleration",
-                 font_size = 10)
-    """
-
-    # create a copy of the dataframe so that original dataframe
-    #  remains unchanged
-    df = data.copy(deep=True)
 
     # Defensive programming: Check if user provides valid inputs
 
-    #      # Ensure color variable is specified by the user
-    #     if color is None:
-    #         raise ValueError("Variable color needs to be specified.")
-
     # Ensure title variable is specified by the user
-    if title is None:
-        raise ValueError("Variable title needs to be specified.")
+    assert title is not None, "Variable title needs to be specified."
 
     error_one = "TypeError: Data must be entered as a pandas dataframe."
-    assert isinstance(df, pd.DataFrame), error_one
+    assert isinstance(data, pd.DataFrame), error_one
 
     error_two = "Wrong type! X-axis variable must be entered as a String."
     assert isinstance(xval, str), error_two
 
-    error_three = "Wrong type! X-axis variable must be entered as a String."
+    error_three = "Wrong type! Y-axis variable must be entered as a String."
     assert isinstance(yval, str), error_three
 
     error_four = "TypeError: x_transform must be of type boolean."
@@ -120,7 +51,7 @@ def numerical_plot(
     error_seven = "TypeError: plot_width must be an integer."
     assert isinstance(plot_width, int), error_seven
 
-    error_eight = "TypeError: plot_height must be a positive integer"
+    error_eight = "TypeError: plot_height must be an integer."
     assert isinstance(plot_height, int), error_eight
 
     error_nine = "TypeError: font_size must be a positive integer."
@@ -135,12 +66,14 @@ def numerical_plot(
     error_twelve = "Wrong type!: color_scheme must be a string."
     assert isinstance(color_scheme, str), error_twelve
 
-    # Ensure variable exists in the dataframe
-    if xval not in df.columns:
-        raise ValueError("Variable xval not found in input dataframe.")
+    # create a copy of the dataframe so that original dataframe
+    #  remains unchanged
+    df = data.copy(deep=True)
 
-    if yval not in df.columns:
-        raise ValueError("Variable yval not found in input dataframe.")
+    # Ensure variable exists in the dataframe
+    assert xval in df.columns, "Variable xval not found in input dataframe."
+
+    assert yval in df.columns, "Variable yval not found in input dataframe."
 
     # Ensure variable is numeric
     error_msg_x = "Your x-variable needs to be numeric."
@@ -149,21 +82,13 @@ def numerical_plot(
     error_msg_y = "Your y-variable needs to be numeric."
     assert pd.api.types.is_numeric_dtype(df[yval]), error_msg_y
 
-    #     # Ensure color variable exists in the dataframe
-    #     if color not in df.columns:
-    #         raise ValueError("Variable color not found in input dataframe.")
-
-    # Check if need to add this
-    #     # Ensure color variable is categorical
-    #     error_msg_x = "Your color variable needs to be cate."
-    #     assert pd.api.types.is_numeric_dtype(df[xval]), error_msg_x
-
     # Toggle a log transformation on the x-axis
     warn_one = "Can't have negative x values with np.log"
     if x_transform:
         if any(df[xval] < 0):
             warnings.warn(warn_one)
         df[xval] = np.log(df[xval])
+        xval = xval
 
     # Toggle a log transformation on the y-axis
     warn_two = "Can't have negative y values with np.log"
@@ -187,7 +112,7 @@ def numerical_plot(
     # Code for the scatter plot
     if plot_type == "scatter":
         if color in df.columns:
-            numerical_plot = (
+            numerical_eda = (
                 alt.Chart(df, title=alt.TitleParams(text=title))
                 .mark_circle(size=30, opacity=0.8)
                 .encode(
@@ -200,7 +125,7 @@ def numerical_plot(
                 titleFontSize=font_size, labelFontSize=font_size, labelAngle=0
             )
         else:
-            numerical_plot = (
+            numerical_eda = (
                 alt.Chart(df, title=alt.TitleParams(text=title))
                 .mark_circle(size=30, opacity=0.8, color=color)
                 .encode(
@@ -215,7 +140,7 @@ def numerical_plot(
     # Code for the line plot
     elif plot_type == "line":
         if color in df.columns:
-            numerical_plot = (
+            numerical_eda = (
                 alt.Chart(df, title=alt.TitleParams(text=title))
                 .mark_line(size=1, opacity=0.8)
                 .encode(
@@ -229,7 +154,7 @@ def numerical_plot(
                 )
             )
         else:
-            numerical_plot = (
+            numerical_eda = (
                 alt.Chart(df, title=alt.TitleParams(text=title))
                 .mark_line(size=1, opacity=0.8, color=color)
                 .encode(
@@ -242,4 +167,4 @@ def numerical_plot(
                 )
             )
 
-    return numerical_plot
+    return numerical_eda
--- tests/test_numerical_eda.py
+++ tests/test_numerical_eda.py
@@ -4,15 +4,33 @@ import pandas as pd
 from vega_datasets import data
 import warnings
 import pytest
-from simpler_eda.numerical_eda import numerical_plot
+from simpler_eda.numerical_eda import numerical_eda
 
 cars = data.cars()
 
 
-def test_numerical_plot():
+@pytest.fixture
+def test_negative():
+    negative_data = pd.DataFrame(
+        {"x": [1, 2, 6, 7, 83, -1], "y": [1, 25, 6, -77, 2, 3]}
+    )
+    return negative_data
+
+
+def test_numerical_eda():
+
+    """
+    Tests the numerical eda function numerical_eda to make sure the outputs are
+    correctly rendering.
+
+    Returns:
+    --------
+    None
+        The test should pass and no asserts should be displayed.
+    """
 
     # Unit test for a scatter plot with no transformations and using mark color
-    plot1 = numerical_plot(
+    plot1 = numerical_eda(
         cars,
         xval="Horsepower",
         yval="Acceleration",
@@ -40,7 +58,7 @@ def test_numerical_plot():
     assert plot1.mark.color == "red", "The color of the mark should be red"
 
     # Unit test for a line plot with no transformations and using mark color
-    plot2 = numerical_plot(
+    plot2 = numerical_eda(
         cars,
         xval="Acceleration",
         yval="Displacement",
@@ -68,7 +86,7 @@ def test_numerical_plot():
     assert plot2.mark.color == "blue", "The color of the mark should be blue"
 
     # Unit test for a scatter plot with transformations and using color with categorical
-    plot3 = numerical_plot(
+    plot3 = numerical_eda(
         cars,
         xval="Displacement",
         yval="Acceleration",
@@ -100,11 +118,11 @@ def test_numerical_plot():
     ), "The color of the groupings should be origin"
 
     # Unit test for a line plot with transformations and using color with numerical variable
-    plot4 = numerical_plot(
+    plot4 = numerical_eda(
         cars,
         xval="Displacement",
         yval="Acceleration",
-        color="Horsepower",
+        color="Origin",
         title="Lineplot",
         plot_type="line",
         font_size=10,
@@ -128,5 +146,343 @@ def test_numerical_plot():
     assert plot4.mark["type"] in ["line"], "the plot type (mark) should be a line"
     assert plot4.title.text == "Lineplot", "The plot title should be Scatterplot"
     assert (
-        plot4.encoding.color.field == "Horsepower"
-    ), "The color of the groupings should be Horsepower"
+        plot4.encoding.color.field == "Origin"
+    ), "The color of the groupings should be Origin"
+
+
+def test_input_type():
+    """
+    Test for error if input is of a wrong type
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    None
+        The test should pass and no asserts should be displayed.
+    """
+
+    # Wrong dataframe input
+    try:
+        numerical_eda(
+            list(cars),
+            xval="Horsepower",
+            yval="Displacement",
+            color="Origin",
+            title="Plot",
+            plot_type="line",
+            font_size=10,
+            color_scheme="purpleorange",
+            plot_height=200,
+            plot_width=400,
+        )
+    except Exception as e:
+        assert str(e) == "TypeError: Data must be entered as a pandas dataframe."
+
+    # Wrong input for xval, must be string
+    try:
+        numerical_eda(
+            cars,
+            xval=10,
+            yval="Displacement",
+            color="Origin",
+            title="Plot",
+            plot_type="line",
+            font_size=10,
+            color_scheme="purpleorange",
+            plot_height=200,
+            plot_width=400,
+        )
+    except Exception as e:
+        assert str(e) == "Wrong type! X-axis variable must be entered as a String."
+
+    # Wrong input for yval, must be string
+    try:
+        numerical_eda(
+            cars,
+            xval="Horsepower",
+            yval=20,
+            color="Origin",
+            title="Plot",
+            plot_type="line",
+            font_size=10,
+            color_scheme="purpleorange",
+            plot_height=200,
+            plot_width=400,
+        )
+    except Exception as e:
+        assert str(e) == "Wrong type! Y-axis variable must be entered as a String."
+
+    # Wrong input type for x_transform, must be boolean
+    try:
+        numerical_eda(
+            cars,
+            xval="Horsepower",
+            yval="Displacement",
+            color="Origin",
+            title="Plot",
+            plot_type="line",
+            font_size=10,
+            color_scheme="purpleorange",
+            plot_height=200,
+            plot_width=400,
+            x_transform="False",
+        )
+    except Exception as e:
+        assert str(e) == "TypeError: x_transform must be of type boolean."
+
+    # Wrong input type for y_transform, must be boolean
+    try:
+        numerical_eda(
+            cars,
+            xval="Horsepower",
+            yval="Displacement",
+            color="Origin",
+            title="Plot",
+            plot_type="line",
+            font_size=10,
+            color_scheme="purpleorange",
+            plot_height=200,
+            plot_width=400,
+            y_transform="False",
+        )
+    except Exception as e:
+        assert str(e) == "TypeError: y_transform must be of type boolean."
+
+    # Wrong input type for plot_width, must be integer
+    try:
+        numerical_eda(
+            cars,
+            xval="Horsepower",
+            yval="Displacement",
+            color="Origin",
+            title="Plot",
+            plot_type="line",
+            font_size=10,
+            color_scheme="purpleorange",
+            plot_height=200,
+            plot_width="Horse",
+            x_transform=False,
+        )
+    except Exception as e:
+        assert str(e) == "TypeError: plot_width must be an integer."
+
+    # Wrong input type for plot_height, must be integer
+    try:
+        numerical_eda(
+            cars,
+            xval="Horsepower",
+            yval="Displacement",
+            color="Origin",
+            title="Plot",
+            plot_type="line",
+            font_size=10,
+            color_scheme="purpleorange",
+            plot_height="200",
+            plot_width=100,
+            x_transform=False,
+        )
+    except Exception as e:
+        assert str(e) == "TypeError: plot_height must be an integer."
+
+    # Wrong input type for font_size, must be integer
+    try:
+        numerical_eda(
+            cars,
+            xval="Horsepower",
+            yval="Displacement",
+            color="Origin",
+            title="Plot",
+            plot_type="line",
+            font_size="10",
+            color_scheme="purpleorange",
+            plot_height=200,
+            plot_width=100,
+            x_transform=False,
+        )
+    except Exception as e:
+        assert str(e) == "TypeError: font_size must be a positive integer."
+
+    # Wrong input type for color, must be string
+    try:
+        numerical_eda(
+            cars,
+            xval="Horsepower",
+            yval="Displacement",
+            color=50,
+            title="Plot",
+            plot_type="line",
+            font_size=10,
+            color_scheme="purpleorange",
+            plot_height=200,
+            plot_width=100,
+            x_transform=False,
+        )
+    except Exception as e:
+        assert str(e) == "Wrong type!: color must be a string."
+
+    # Wrong input type for title, must be string
+    try:
+        numerical_eda(
+            cars,
+            xval="Horsepower",
+            yval="Displacement",
+            color="red",
+            title=list("deep"),
+            plot_type="line",
+            font_size=10,
+            color_scheme="purpleorange",
+            plot_height=200,
+            plot_width=100,
+            x_transform=False,
+        )
+    except Exception as e:
+        assert str(e) == "Wrong type!: title must be a string."
+
+    # Wrong input type for color_scheme, must be string
+    try:
+        numerical_eda(
+            cars,
+            xval="Horsepower",
+            yval="Displacement",
+            color="red",
+            title="Plot",
+            plot_type="line",
+            font_size=10,
+            color_scheme=list("purpleorange"),
+            plot_height=200,
+            plot_width=100,
+            x_transform=False,
+        )
+    except Exception as e:
+        assert str(e) == "Wrong type!: color_scheme must be a string."
+
+
+def test_input_value():
+    """
+    Test for error if input is of a wrong value
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    None
+        The test should pass and no asserts should be displayed.
+    """
+
+    # Test for whether xval is in the dataframe
+    try:
+        numerical_eda(
+            cars,
+            xval="Horse",
+            yval="Displacement",
+            color="Origin",
+            title="Plot",
+            plot_type="line",
+            font_size=10,
+            color_scheme="purpleorange",
+            plot_height=200,
+            plot_width=400,
+        )
+    except Exception as e:
+        assert str(e) == "Variable xval not found in input dataframe."
+
+    # Test for whether yval is in the dataframe
+    try:
+        numerical_eda(
+            cars,
+            xval="Horsepower",
+            yval="Displa",
+            color="Origin",
+            title="Plot",
+            plot_type="line",
+            font_size=10,
+            color_scheme="purpleorange",
+            plot_height=200,
+            plot_width=400,
+        )
+    except Exception as e:
+        assert str(e) == "Variable yval not found in input dataframe."
+
+    # Test for whether Title is not  specified
+    try:
+        numerical_eda(
+            cars,
+            xval="Horsepower",
+            yval="Displacement",
+            color="Origin",
+            title=None,
+            plot_type="line",
+            font_size=10,
+            color_scheme="purpleorange",
+            plot_height=200,
+            plot_width=400,
+        )
+    except Exception as e:
+        assert str(e) == "Variable title needs to be specified."
+
+
+def test_negX(test_negative):
+    """
+    Test whether the function returns warning for negative values of xval for log transformation
+
+    Parameters
+    ----------
+    test_negative: A pandas dataframe
+
+    Returns
+    -------
+    None
+        The test should pass and no asserts should be displayed.
+    ----------
+    """
+    with pytest.warns(UserWarning):
+        numerical_eda(
+            test_negative,
+            xval="x",
+            yval="y",
+            color="red",
+            title="Plot",
+            plot_type="scatter",
+            font_size=10,
+            color_scheme="purpleorange",
+            plot_height=200,
+            plot_width=400,
+            x_transform=True,
+            y_transform=False,
+        )
+
+
+def test_negY(test_negative):
+    """
+    Test whether the function returns warning for negative values of yval for log transformation
+
+    Parameters
+    ----------
+    test_negative: A pandas dataframe
+
+    Returns
+    -------
+    None
+        The test should pass and no asserts should be displayed.
+    ----------
+    """
+    with pytest.warns(UserWarning):
+        numerical_eda(
+            test_negative,
+            xval="x",
+            yval="y",
+            color="red",
+            title="Plot",
+            plot_type="scatter",
+            font_size=10,
+            color_scheme="purpleorange",
+            plot_height=200,
+            plot_width=400,
+            x_transform=False,
+            y_transform=True,
+        )
